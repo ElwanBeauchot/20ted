14aa121013ee7397849144bda15ddfcf9073a469
Temayoo <Matt.yo95280@gmail.com>
2024-10-04 09:48:38 +0200

    show discussion create service

--- src/Repository/MessageRepository.php
+++ src/Repository/MessageRepository.php
@@ -40,19 +40,4 @@ class MessageRepository extends ServiceEntityRepository
         return array_values($lastMessages);
     }
 
-    public function showDiscussion($productId, $user1Id, $user2Id)
-    {
-        $qb = $this->createQueryBuilder('m')
-            ->select('m')
-            ->join('m.products', 'p')
-            ->join('p.users', 'u')
-            ->where('p.id = :productId')
-            ->andWhere('(m.buyer = :user1 AND u.id = :user2) OR (m.buyer = :user2 AND u.id = :user1)')
-            ->setParameter('productId', $productId)
-            ->setParameter('user1', $user1Id)
-            ->setParameter('user2', $user2Id)
-            ->orderBy('m.date', 'ASC');
-
-        return $qb->getQuery()->getResult();
-    }
 }
